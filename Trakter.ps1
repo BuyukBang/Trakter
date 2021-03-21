02e0c1411185660a9255e9b387ba19f18b550202
buyukbang <buyukbang@users.noreply.github.com>
2021-03-21 14:31:32 +0300

    Add files via upload

--- Trakter.ps1
+++ Trakter.ps1
@@ -14,28 +14,28 @@ function DoJobs {
 
 
 function UpdateList() {
-	$FlexgetParams=$args[0]
-	$UrlParams=$args[1]
-	echo "Starting update for --> $FlexgetParams $UrlParams"
+  $FlexgetParams=$args[0]
+  $UrlParams=$args[1]
+  echo "Starting update for --> $FlexgetParams $UrlParams"
 
-	#Clean up	
-	Remove-Item imdb.txt -ErrorAction Ignore
+  #Clean up
+  Remove-Item imdb.txt -ErrorAction Ignore
   iex 'flexget database reset-plugin discover'
   iex 'flexget movie-list purge All'
   iex 'flexget movie-list purge Releases'
   iex 'flexget movie-list purge NoReleases'
   iex 'flexget movie-list purge ReleasesWithPossibleDuplicates'
 
-	#Download IMDB List
-	$MovieCount=0
-	$MovieCountPrevious=-1
-	$StartValue=0
-	#Comparing with a high value to be sure about infinite loops on error cases
-	while(($MovieCount -gt $MovieCountPrevious) -and ($MovieCount -lt 2000) -and ($MovieCount%250 -eq 0)) 
+  #Download IMDB List
+  $MovieCount=0
+  $MovieCountPrevious=-1
+  $StartValue=0
+  #Comparing with a high value to be sure about infinite loops on error cases
+  while(($MovieCount -gt $MovieCountPrevious) -and ($MovieCount -lt 2000) -and ($MovieCount%250 -eq 0)) 
   {
-  	echo "Getting movie list from IMDB at $StartValue for --> $FlexgetParams"
-  	$MovieCountPrevious=$MovieCount
-	  $Url='https://www.imdb.com/search/title/?title_type=feature,tv_movie,documentary,short,video&view=simple&count=250&start=' + $StartValue + '&' + $UrlParams
+    echo "Getting movie list from IMDB at $StartValue for --> $FlexgetParams"
+    $MovieCountPrevious=$MovieCount
+    $Url='https://www.imdb.com/search/title/?title_type=feature,tv_movie,documentary,short,video&view=simple&count=250&start=' + $StartValue + '&' + $UrlParams
     $r=Invoke-WebRequest -Uri $Url
     $r.content.split("`r`n") | select-string -Pattern 'tt\d{7,8}' -AllMatches | foreach {$_.matches.value} | sort | get-unique >> imdb.txt
     $MovieCount=(Get-Content -ErrorAction Ignore imdb.txt).Length
@@ -48,7 +48,7 @@ function UpdateList() {
   $cmd='flexget execute -v --no-cache --discover-now --cli-config ' + $FlexgetParams
   iex $cmd
   
-	echo "Completed update for --> $FlexgetParams $UrlParams"
+  echo "Completed update for --> $FlexgetParams $UrlParams"
 }
 
 
